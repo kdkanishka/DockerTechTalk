74202877e8d4d707594762ddeb5959f3c70f97c6
Kanishka <Kanishka Dilshan>
2015-03-12 15:08:29 +0530

    Added logging capability

--- pythonscripts/request.py
+++ pythonscripts/request.py
@@ -9,4 +9,6 @@ while True:
 	r = requests.get('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new', auth=('user', 'pass'))
 	#print r.status_code
 	print r.text
+	with open("/var/log/requester.log", "a") as myfile:
+		myfile.write(r.text)
 
